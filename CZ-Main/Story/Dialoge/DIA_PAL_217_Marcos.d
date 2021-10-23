a1e1fbca50625442e89dd75e4704ebdcdae153f5
Nestefix <91119755+Nestefix@users.noreply.github.com>
2021-10-23 19:03:36 +0200

    Update DIA_PAL_217_Marcos.d

--- CZ-Main/Story/Dialoge/DIA_PAL_217_Marcos.d
+++ CZ-Main/Story/Dialoge/DIA_PAL_217_Marcos.d
@@ -42,12 +42,12 @@ func int DIA_Marcos_Hallo_Condition()
 
 func void DIA_Marcos_Hallo_Info()
 {
-	AI_Output(self,other,"DIA_Marcos_Hallo_04_00");	//Stůj! Ve jménu Innose! Jsem královský paladin! Řekni kdo jsi a co tu chceš! A pravdu!
+	AI_Output(self,other,"DIA_Marcos_Hallo_04_00");	//Stůj! Ve jménu Innose! Jsem Marcos, královský paladin! Řekni kdo jsi a co tu chceš! A pravdu!
 
 	if(other.guild == GIL_KDF)
 	{
 		AI_Output(other,self,"DIA_Marcos_Hallo_15_01");	//Innosův vyvolený mluví vždy pravdu.
-		AI_Output(self,other,"DIA_Marcos_Hallo_04_02");	//Odpust ctihodný mágu. Neuvědomil jsem si s kým mluvím!
+		AI_Output(self,other,"DIA_Marcos_Hallo_04_02");	//Odpusť ctihodný mágu. Neuvědomil jsem si s kým mluvím!
 		AI_Output(other,self,"DIA_Marcos_Hallo_15_03");	//Nevadí.
 		AI_Output(self,other,"DIA_Marcos_Hallo_04_04");	//Jestli se můžu zeptat - Co tě sem přivádí?
 	}
@@ -58,12 +58,12 @@ func void DIA_Marcos_Hallo_Info()
 	}
 	else if(other.guild == GIL_SLD)
 	{
-		AI_Output(other,self,"DIA_Marcos_Hallo_15_07");	//Klídek, máme stejného šéfa -lorda Hagena.
+		AI_Output(other,self,"DIA_Marcos_Hallo_15_07");	//Klídek, máme stejného šéfa - lorda Hagena.
 		AI_Output(self,other,"DIA_Marcos_Hallo_04_08");	//Hagen najmul žoldáka? Mluv, co tu chceš?!
 	}
 	else
 	{
-		AI_Output(other,self,"DIA_Marcos_Hallo_15_07");	//Klídek, máme stejného šéfa -lorda Hagena.
+		AI_Output(other,self,"DIA_Marcos_Hallo_15_07");	//Klídek, máme stejného šéfa - lorda Hagena.
 		AI_Output(self,other,"DIA_Marcos_Hallo_04_08");	//Hagen najmul žoldáka? Mluv, co tu chceš?!
 	};
 };
@@ -76,7 +76,7 @@ instance DIA_Marcos_Hagen(C_Info)
 	condition = DIA_Marcos_Hagen_Condition;
 	information = DIA_Marcos_Hagen_Info;
 	permanent = FALSE;
-	description = "Mám lordu přinést důkaz o přítomnosti draků.";
+	description = "Mám lordu Hagenovi přinést důkaz o přítomnosti draků.";
 };
 
 
@@ -90,11 +90,11 @@ func int DIA_Marcos_Hagen_Condition()
 
 func void DIA_Marcos_Hagen_Info()
 {
-	AI_Output(other,self,"DIA_Marcos_Hagen_15_00");	//Mám lordu přinést důkaz o přítomnosti draků.
+	AI_Output(other,self,"DIA_Marcos_Hagen_15_00");	//Mám lordu Hagenovi přinést důkaz o přítomnosti draků.
 	AI_Output(self,other,"DIA_Marcos_Hagen_04_01");	//Pak tu neztrácej čas. Zbytečně riskuješ život.
 	AI_Output(self,other,"DIA_Marcos_Hagen_04_02");	//Myslíš si, že tady najdeš dračí šupiny a doneseš mu je?
 	AI_Output(self,other,"DIA_Marcos_Hagen_04_03");	//Zkus se dostat do hradu a promluv s Garondem.
-	AI_Output(self,other,"DIA_Marcos_Hagen_04_04");	//Musí se dozvědět, že jsi na misi pro lorda Hagena. Myslím, že ti dá ten tvů důkaz...
+	AI_Output(self,other,"DIA_Marcos_Hagen_04_04");	//Musí se dozvědět, že jsi na misi pro lorda Hagena. Myslím, že ti dá ten tvůj důkaz...
 };
 
 
@@ -121,15 +121,15 @@ func void DIA_Marcos_Garond_Info()
 {
 	AI_Output(other,self,"DIA_Marcos_Garond_15_00");	//Přicházím od Garonda. Chce vědět, kolik jste vydolovali rudy.
 	AI_Output(self,other,"DIA_Marcos_Garond_04_01");	//Řekni Garondovi, že jsme dopravili rudu do bezpečí, protože se tam začali ukazovat skřeti.
-	AI_Output(self,other,"DIA_Marcos_Garond_04_02");	//Zkoušeli jsem se dostat do pevnosti, ale nakonec jsem přežil jen já.
+	AI_Output(self,other,"DIA_Marcos_Garond_04_02");	//Zkoušeli jsme se dostat do pevnosti, ale nakonec jsem přežil jen já.
 	AI_Output(self,other,"DIA_Marcos_Garond_04_03");	//Schoval jsem rudu na bezpečném místě máme tam 4 bedny. Jdi a řekni to Garondovi.
 	AI_Output(self,other,"DIA_Marcos_Garond_04_04");	//Řekni mu, že bráním rudu svým životem, ale že nevím, jak dlouho bude trvat, než mě najdou a přemůžou.
 	AI_Output(self,other,"DIA_Marcos_Garond_04_05");	//Řekni mu, že potřebuji nějaké posily.
 	AI_Output(other,self,"DIA_Marcos_Garond_15_06");	//Dám mu vědět.
-	B_LogEntry(TOPIC_ScoutMine,"Paladin Markus ochraňuje čtyři bedny rudy v malém údolíčku vedle pevnosti.");
+	B_LogEntry(TOPIC_ScoutMine,"Paladin Marcos ochraňuje čtyři bedny rudy v malém údolíčku vedle pevnosti.");
 	Log_CreateTopic(Topic_MarcosJungs,LOG_MISSION);
 	Log_SetTopicStatus(Topic_MarcosJungs,LOG_Running);
-	B_LogEntry_Quiet(Topic_MarcosJungs,"Markus chce, aby mu Garond poslal posily.");
+	B_LogEntry_Quiet(Topic_MarcosJungs,"Marcos chce, aby mu Garond poslal posily.");
 	MIS_Marcos_Jungs = LOG_Running;
 	Marcos_Ore = TRUE;
 	self.flags = 0;
@@ -170,7 +170,7 @@ func void DIA_Marcos_Perm_Info()
 	}
 	else if(MIS_Marcos_Jungs == LOG_Running)
 	{
-		AI_Output(self,other,"DIA_Marcos_Perm_04_02");	//Věřím a doufám, že ji Garond pošle.
+		AI_Output(self,other,"DIA_Marcos_Perm_04_02");	//Věřím a doufám, že Garond brzy pošle posily.
 	}
 	else if(MIS_Marcos_Jungs == LOG_SUCCESS)
 	{
